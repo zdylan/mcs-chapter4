aa0d8fd73d2855460e7a044850a7de41c8ccf1ab
dylan_zhang_ <dylan.zhang111@gmail.com>
2018-09-21 00:12:37 +0800

    add first version.

---
+++ eurekasvr/src/main/java/com/thoughtmechanix/eurekasvr/EurekasvrApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtmechanix.eurekasvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekasvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekasvrApplication.class, args);
+    }
+}
