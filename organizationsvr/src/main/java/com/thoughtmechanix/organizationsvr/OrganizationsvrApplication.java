b4595be77573c2eebf6156398810542ab72e95c1
dylan_zhang_ <dylan.zhang111@gmail.com>
2018-09-25 23:36:12 +0800

    add service organization.

---
+++ organizationsvr/src/main/java/com/thoughtmechanix/organizationsvr/OrganizationsvrApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtmechanix.organizationsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationsvrApplication.class, args);
+    }
+}
