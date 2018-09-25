a6ed16baf624d07dfcbe88f9b7807ba3965c7dde
dylan_zhang_ <dylan.zhang111@gmail.com>
2018-09-25 23:48:41 +0800

    add docker for organization server.

--- eurekasvr/src/main/docker/run.sh
+++ eurekasvr/src/main/docker/run.sh
@@ -2,6 +2,6 @@
 echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-#java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
 
-java -jar /usr/local/eurekaserver/@project.build.finalName@.jar
+#java -jar /usr/local/eurekaserver/@project.build.finalName@.jar
