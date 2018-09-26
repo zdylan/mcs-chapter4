600e33397f4c27160358967eb41c8e65c9d1f241
zhang.gai <gai.zhang@nx-engine.com>
2018-09-26 14:02:51 +0800

    add config service.

---
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtmechanix.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
